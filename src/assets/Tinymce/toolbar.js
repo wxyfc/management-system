bbcaf1c66abe49d13964ecce616c7c47095c8090
user-yun <xuyunfei.mail@qq.com>
2020-03-23 18:30:45 +0800

    版本0.4.1：增加百度地图，增加富文本编辑器，准备自动滚动视图等。

---
+++ src/assets/Tinymce/toolbar.js
@@ -0,0 +1,8 @@
+// Here is a list of the toolbar
+// Detail list see https://www.tinymce.com/docs/advanced/editor-control-identifiers/#toolbarcontrols
+
+const toolbar = [ 'newdocument searchreplace bold italic underline strikethrough alignleft aligncenter alignright alignjustify styleselect formatselect fontselect fontsizeselect fullscreen' ,
+    ' cut copy paste outdent indent  blockquote undo redo removeformat subscript superscript code codesample hr bullist numlist link image charmap preview anchor pagebreak insertdatetime ' ,
+    ' media table emoticons forecolor backcolor ' ]
+
+export default toolbar
---
+++ src/views/business/richtext/tinymce/Tinymce.vue
@@ -0,0 +1,36 @@
+<template>
+    <el-row>
+        <mt>{{language[$options.name]}}</mt>
+        <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12" style="height: 80%;" class="padding1vw">
+            <Tinymce v-model="content" :height="660"/>
+        </el-col>
+        <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12" style="height: 96%;" class="padding1vw">
+            <div v-html="content" class="overflowauto h100"></div>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+    export default {
+        mixins : [ require ( "@/mymixins" ).default ] ,
+        name : "tinymceDemo" ,
+        data () {
+            return {
+                content :
+                    `<h1 style="text-align: center;">Welcome to the TinyMCE demo!</h1><p style="text-align: center; font-size: 15px;">
+                    <img title="TinyMCE Logo" src="//www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE Logo" width="110" height="97" />
+                    <ul>
+                        <li>Our <a href="//www.tinymce.com/docs/">documentation</a> is a great resource for learning how to configure TinyMCE.</li>
+                        <li>Have a specific question? Visit the <a href="https://community.tinymce.com/forum/">Community Forum</a>.</li>
+                        <li>We also offer enterprise grade support as part of <a href="https://tinymce.com/pricing">TinyMCE premium subscriptions</a>.</li>
+                    </ul>`
+            };
+        } ,
+        components : {
+            Tinymce : () => import('@/assets/Tinymce')
+        } ,
+        methods : {} ,
+        mounted () {
+        } ,
+    };
+</script>
